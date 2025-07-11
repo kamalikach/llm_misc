aebaa02b96f2277d770ffd4b473d6e27104eb2ea
kamalikach <kamalika@gmail.com>
2025-07-11 13:54:47 +0000

    modified FT

--- llm_chat.py
+++ llm_chat.py
@@ -1,13 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from LlamaModel import *
+from LlamaModelFT import *
 from BaseModel import *
 import torch
 
-model_id = "meta-llama/Llama-3.1-8B-Instruct"  # Example for LLaMA 3.1 8B model
+model_id = "meta-llama/Meta-Llama-3-8b-Instruct"
+ckpt_dir = "/data/pengrun/dia_models/propInfer_series/Llama3-CC/data_30_6500_rs_300_Llama-3-8b_input"
 
-llama = LlamaModel(model_id)
-device = "cuda:1" if torch.cuda.is_available() else "cpu"
-_ = llama.load(device)
+llama = LlamaModelFT(model_id)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+_ = llama.load(ckpt_dir, device)
 
 
 # Chat loop
@@ -19,9 +20,9 @@ while True:
     prompt = llama.format_prompt(user_input)
     tokenized_prompt = llama.tokenizer(prompt, return_tensors='pt').to(llama.device)
     
-    tokenized_response = llama.model.generate(**tokenized_prompt, max_new_tokens=300, top_p=0.95, do_sample=True)
+    tokenized_response = llama.model.generate(**tokenized_prompt, max_new_tokens=300, top_p=0.95, do_sample=True, pad_token_id=llama.tokenizer.eos_token_id)
     full_response = llama.tokenizer.decode(tokenized_response[0], skip_special_tokens = True)
     response = llama.extract_response(full_response)
     llama.update_chat_history(user_input, response)
-    print("Llama:", response)
+    print("AI:", response)
 
