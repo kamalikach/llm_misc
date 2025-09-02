94a28d67562bb54c5ef0929c6a6acef7ab6f4714
kamalikach <kamalika@gmail.com>
2025-09-02 23:22:41 +0000

    edits

---
+++ llm_chat_temperature.py
@@ -0,0 +1,36 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from BaseModel import *
+from LlamaModel import *
+import argparse
+import torch
+
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+llama = LlamaModel(model_id)
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+_ = llama.load(device)
+
+
+# Chat loop
+def main():
+    parser = argparse.ArgumentParser(description="LLM chat with different temperatures")
+    parser.add_argument("--temperature", type=float, default=1.0,
+                        help="Sampling temperature (default: 1.0)")
+
+    args = parser.parse_args()
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit"]:
+            break
+
+        prompt = llama.format_prompt(user_input)
+        tokenized_prompt = llama.tokenizer(prompt, return_tensors='pt').to(llama.device)
+    
+        tokenized_response = llama.model.generate(**tokenized_prompt, max_new_tokens=300, temperature=args.temperature, do_sample=True, pad_token_id=llama.tokenizer.eos_token_id)
+        full_response = llama.tokenizer.decode(tokenized_response[0], skip_special_tokens = True)
+        response = llama.extract_response(full_response)
+        llama.update_chat_history(user_input, response)
+        print("AI:", response)
+
+if __name__ == "__main__":
+    main()
